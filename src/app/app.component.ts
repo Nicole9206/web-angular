d210d2b129a31d8a9ad083ed030e2feafa78291e
Nicole Latorre <nylatorre@gmail.com>
2022-08-08 11:44:31 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a web angular';
+
+  curso: string = 'Página basada en angular y spring boot';
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,6 @@
+
+import {Cliente} from './cliente';
+
+export const CLIENTES:Cliente[] = [
+  {id: 1, nombre: 'Nicole', apellido: 'Latorre', email: 'nicole.latorrefig@gmail.com', createAt: '2012-07-08'}
+];
